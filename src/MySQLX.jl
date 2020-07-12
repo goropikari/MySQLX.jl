56d9cfa0aea7d7e0a273f5ba832b5de2b05fcdab
goropikari <goropikari56@gmail.com>
2020-07-12 23:19:45 +0900

    Define enum values as dictionary

--- src/MySQLX.jl
+++ src/MySQLX.jl
@@ -10,15 +10,27 @@ const Session   = Mysqlx.Session
 const Sql       = Mysqlx.Sql
 const Resultset = Mysqlx.Resultset
 
-SESS_AUTHENTICATE_START = 4
-SESS_AUTHENTICATE_CONTINUE  = 5
+const ClientMessagesType = let
+    _type_names = fieldnames(Mysqlx.__enum_ClientMessages_Type)
+    Dict(type => getproperty(Mysqlx.ClientMessages_Type, type) for type in _type_names)
+end
+
+const ServerMessagesType = let
+    _type_names = fieldnames(Mysqlx.__enum_ServerMessages_Type)
+    Dict(type => getproperty(Mysqlx.ServerMessages_Type, type) for type in _type_names)
+end
+
+const ColumnMetaDataType = let
+    _type_names = fieldnames(Resultset.__enum_ColumnMetaData_FieldType)
+    Dict(type => getproperty(Resultset.ColumnMetaData_FieldType, type) for type in _type_names)
+end
 
 function _read_packet(sock)
     len_little_endian = read(sock, 4)
     len = reinterpret(Int32, len_little_endian)[] - 1
     typ = read(sock, 1)[]
     payload = read(sock, len)
-    if (typ == 0x0b) # Notice
+    if (typ == ServerMessagesType[:NOTICE])
         return _read_packet(sock)
     end
     return typ, payload
@@ -49,11 +61,11 @@ function connect(sock::Sockets.TCPSocket, host::String, user::String, password::
     # C -> S: SESS_AUTHENTICATE_START
     auth = Session.AuthenticateStart(mech_name = "MYSQL41")
     payload = _serialize_to_string(auth)
-    _client_to_server(sock, SESS_AUTHENTICATE_START, payload)
+    _client_to_server(sock, ClientMessagesType[:SESS_AUTHENTICATE_START], payload)
 
     # S -> C: AuthenticateContinue
     typ, payload = _read_packet(sock)
-    auth_conti = MySQLX.Session.AuthenticateContinue()
+    auth_conti = Session.AuthenticateContinue()
     ProtoBuf.readproto(PipeBuffer(payload), auth_conti)
 
     # C -> S: AuthenticateContinue
@@ -67,54 +79,53 @@ function connect(sock::Sockets.TCPSocket, host::String, user::String, password::
     iob = PipeBuffer()
     write(iob, user)
     auth_data = vcat([0x00], iob.data, [0x00, 0x2a], auth_data, [0x00])
-    auth_conti = MySQLX.Session.AuthenticateContinue(auth_data = auth_data)
+    auth_conti = Session.AuthenticateContinue(auth_data = auth_data)
     payload = _serialize_to_string(auth_conti)
     len = length(payload) + 1
     len_little_endian = reinterpret(UInt8, [Int32(len)])
-    typ = UInt8(SESS_AUTHENTICATE_CONTINUE)
-    # write(sock, vcat(len_little_endian, typ, payload))
+    typ = UInt8(ClientMessagesType[:SESS_AUTHENTICATE_CONTINUE])
     _client_to_server(sock, typ, payload)
 
-
-    # S -> C: SESS_AUTHENTICATE_OK = 4
+    # S -> C: SESS_AUTHENTICATE_OK: 4
     typ, payload = _read_packet(sock)
-    if typ == 0x04
-        # auth_ok = MySQLX.Session.AuthenticateOk()
+    if typ == ServerMessagesType[:SESS_AUTHENTICATE_OK]
+        # auth_ok = Session.AuthenticateOk()
         # ProtoBuf.readproto(PipeBuffer(payload), auth_ok)
         return Connection(sock)
     else
         error()
     end
 end
-connect(host::String, user::String, password::String; db::String="", port::Integer=33060) = connect(Sockets.connect(host, port), host, user, password, db=db, port=port)
+connect(host::String, user::String, password::String; db::String="", port::Integer=33060) =
+    connect(Sockets.connect(host, port), host, user, password, db=db, port=port)
 
 function execute(conn::Connection, sql)
-    # C -> S: SQL_STMT_EXECUTE = 12
-    stmt_execute = MySQLX.Sql.StmtExecute()
+    # C -> S: SQL_STMT_EXECUTE: 12
+    stmt_execute = Sql.StmtExecute()
     stmt_execute.stmt = UInt8.(transcode(UInt8, sql))
     stmt_execute.compact_metadata = false
     payload = _serialize_to_string(stmt_execute)
     len = Int32(length(payload) + 1)
     len_little_endian = reinterpret(UInt8, [len])
-    SQL_STMT_EXECUTE = 12
-    typ = UInt8(SQL_STMT_EXECUTE)
+    typ = UInt8(ClientMessagesType[:SQL_STMT_EXECUTE])
     _client_to_server(conn.sock, typ, payload)
     _parse_resultset(conn)
 end
 
 function _parse_resultset(conn::Connection)
-    # S -> C: RESULTSET_COLUMN_META_DATA = 12
-    col_names = String[]
     typ, payload = _read_packet(conn.sock)
-    if typ == 17 # SQL_STMT_EXECUTE_OK: UPDATE, DELETE, INSERT, etc...
+    if typ == ServerMessagesType[:SQL_STMT_EXECUTE_OK]
         return nothing
-    else typ == 1 # ERROR
-        err = MySQLX.Mysqlx.Error()
+    elseif typ == ServerMessagesType[:ERROR]
+        err = Mysqlx.Error()
         ProtoBuf.readproto(PipeBuffer(payload), err)
         error(err.msg)
     end
-    while typ == 12
-        resultset_id = MySQLX.Resultset.ColumnMetaData()
+
+    # S -> C: RESULTSET_COLUMN_META_DATA: 12
+    col_names = String[]
+    while typ == ServerMessagesType[:RESULTSET_COLUMN_META_DATA]
+        resultset_id = Resultset.ColumnMetaData()
         ProtoBuf.readproto(PipeBuffer(payload), resultset_id)
         println("type: $(resultset_id._type)")
         println("name: $(String(copy(resultset_id.name)))")
@@ -125,9 +136,8 @@ function _parse_resultset(conn::Connection)
     end
 
     # RESULTSET_ROW = 13
-    # typ, payload = _read_packet(conn.sock)
-    while typ == 13
-        row = MySQLX.Resultset.Row()
+    while typ == ServerMessagesType[:RESULTSET_ROW]
+        row = Resultset.Row()
         ProtoBuf.readproto(PipeBuffer(payload), row)
         for (col, field) in zip(col_names, row.field)
             println("$(col) $(field)")
@@ -136,21 +146,22 @@ function _parse_resultset(conn::Connection)
     end
 
     # RESULTSET_FETCH_DONE = 14
-    typ
+    @assert typ == ServerMessagesType[:RESULTSET_FETCH_DONE]
 
     # SQL_STMT_EXECUTE_OK = 17
     typ, payload = _read_packet(conn.sock)
+    @assert typ == ServerMessagesType[:SQL_STMT_EXECUTE_OK]
 
     nothing
 end
 
 function Base.close(conn::Connection)
-    SESS_CLOSE = 7
-    typ = UInt8(SESS_CLOSE)
+    # SESS_CLOSE = 7
+    typ = UInt8(ClientMessagesType[:SESS_CLOSE])
     _client_to_server(conn.sock, typ, UInt8[])
 
     typ, payload = _read_packet(conn.sock)
-    if typ == 0 # OK
+    if typ == ServerMessagesType[:OK] # OK
         nothing
     else
         println(typ)
--- test/runtests.jl
+++ test/runtests.jl
@@ -6,6 +6,10 @@ using Test
     user = "root"
     password = "test"
     conn = MySQLX.connect(host, user, password, port=33060)
+    MySQLX.execute(conn, "DROP DATABASE foo")
+    MySQLX.execute(conn, "CREATE DATABASE foo")
+    MySQLX.execute(conn, "CREATE TABLE foo.bar (id int, name varchar(10))")
+    MySQLX.execute(conn, "INSERT INTO foo.bar VALUES (1, 'dog'), (2, 'cat'), (10, 'hamster');")
     MySQLX.execute(conn, "select * from foo.bar")
     close(conn)
 end
